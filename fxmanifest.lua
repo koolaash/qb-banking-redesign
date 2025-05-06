654066688bf90619c3c9cb4d412ed336b3b171d8
Damon <73039811+koolaash@users.noreply.github.com>
2025-05-07 01:50:49 +0530

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 author 'Kakarot'
-description 'Allows players to deposit/withdraw money from their bank/job account and create shared accounts'
+description 'Edited by kool_damon'
 version '2.0.0'
 
 shared_scripts {
